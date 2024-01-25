7b9f33d7c2061c001af126570a1aa52843d93d30
jonaw21 <157731173+jonaw21@users.noreply.github.com>
2024-01-25 11:11:33 -0600

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#Initialize the lowest and highest temperature to the current temperature.
-lowest_temp = 0
-highest_temp = 0
-current_temp_F = 0
-current_temp_F = temp_F(input.temperature())
-highest_temp = current_temp_F
-lowest_temp = current_temp_F
-
 # Function to convert Celsius to Fahrenheit.
 def temp_F(C: number):
     # Formula for Celsius to Fahrenheit is F = C * 1.8 + 32.
@@ -13,7 +5,8 @@ def temp_F(C: number):
 
 # Function when button A is pressed to read out the highest temperature recorded.
 def on_button_pressed_a():
-    global highest_temp
+    global current_temp_F, highest_temp
+    current_temp_F = temp_F(input.temperature())
     # Check of the current temperature is higher than the recorded highest temperature.
     if current_temp_F > highest_temp:
         # Update the highest temperature.
@@ -24,7 +17,8 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 # Function when button B is pressed to read out the lowest temperature recorded.
 def on_button_pressed_b():
-    global lowest_temp
+    global current_temp_F, lowest_temp
+    current_temp_F = temp_F(input.temperature())
     # Check if the current temperature is lower that the recorded lowest temperature.
     if current_temp_F < lowest_temp:
         # Update the lowest temperature.
@@ -33,7 +27,17 @@ def on_button_pressed_b():
     basic.show_number(lowest_temp)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+
+
+#Initialize the lowest and highest temperature to the current temperature.
+lowest_temp = 0
+highest_temp = 0
+current_temp_F = 0
+current_temp_F = temp_F(input.temperature())
+highest_temp = current_temp_F
+lowest_temp = current_temp_F
+
 # Function to display the current temperature at 1 minute intervals.
 def on_every_interval():
-    basic.show_number(temp_F(input.temperature()))
+    basic.show_number(current_temp_F)
 loops.every_interval(60000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-// Initialize the lowest and highest temperature to the current temperature.
-let lowest_temp = 0
-let highest_temp = 0
-let current_temp_F = 0
-current_temp_F = temp_F(input.temperature())
-highest_temp = current_temp_F
-lowest_temp = current_temp_F
 //  Function to convert Celsius to Fahrenheit.
 function temp_F(C: number): number {
     //  Formula for Celsius to Fahrenheit is F = C * 1.8 + 32.
@@ -14,6 +7,7 @@ function temp_F(C: number): number {
 //  Function when button A is pressed to read out the highest temperature recorded.
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
+    current_temp_F = temp_F(input.temperature())
     //  Check of the current temperature is higher than the recorded highest temperature.
     if (current_temp_F > highest_temp) {
         //  Update the highest temperature.
@@ -26,6 +20,7 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 //  Function when button B is pressed to read out the lowest temperature recorded.
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
+    current_temp_F = temp_F(input.temperature())
     //  Check if the current temperature is lower that the recorded lowest temperature.
     if (current_temp_F < lowest_temp) {
         //  Update the lowest temperature.
@@ -35,7 +30,14 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     //  Display the lowest temperature.
     basic.showNumber(lowest_temp)
 })
+// Initialize the lowest and highest temperature to the current temperature.
+let lowest_temp = 0
+let highest_temp = 0
+let current_temp_F = 0
+current_temp_F = temp_F(input.temperature())
+highest_temp = current_temp_F
+lowest_temp = current_temp_F
 //  Function to display the current temperature at 1 minute intervals.
 loops.everyInterval(60000, function on_every_interval() {
-    basic.showNumber(temp_F(input.temperature()))
+    basic.showNumber(current_temp_F)
 })
